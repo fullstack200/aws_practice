449f30a971340f7a86ddd6180a9158c79cc15cde
Ahmed <fullstackdeveloper404@gmail.com>
2025-11-10 22:26:44 +0530

    Made changes in buildspec.yml and lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -2,5 +2,5 @@ def handler(event, context):
     return {
         "statusCode": 200,
         # change this between commits to observe traffic shift
-        "body": "hello from version X"
+        "body": "hello from version Y"
     }
